f752f946c997befdb09fb5f3dde5152ae4eb6dcb
Andrea <andrea.colucci@live.it>
2025-10-23 19:04:19 +0200

    Simplified players scripts with the use of collisions

--- scripts/BlueDong.cs
+++ scripts/BlueDong.cs
@@ -7,81 +7,46 @@ namespace Dong
 	{
 		[Export]
 		public float Speed { get; set; } = 300.0f;
-		[Export]
-		public bool StopOnWall = true;
-		private int _pixelBuffer = 4;
-		private float _SHSound_lastDirection = 0;
-		private bool _SHSound_played = false;
-		private bool _shootingStarsPlayed = false;
+		private Vector2 _inputDir = Vector2.Zero;
+    	private bool _touchingWall = false;
+    	private float _lastInputDirY = 0;
 
 		public override void _PhysicsProcess(double delta)
 		{
-			float halfViewportHeight = GetViewport().GetVisibleRect().Size.Y / 2;
-			float halfNodeHeight = GetChild<Sprite2D>(0, true).Texture.GetHeight() * GetChild<Sprite2D>(0, true).Transform.Scale.Y * Transform.Scale.Y / 2;
-
-			float availableSpace = halfViewportHeight - halfNodeHeight;
-
-			float currentYPosition = Position.Y;
-			Vector2 currentVelocity = Velocity;
-
-			float currentYDirection = Input.GetAxis("blue_move_up", "blue_move_down");
-			if (currentYDirection != 0)
+			_inputDir.Y = Input.GetAxis("blue_move_up", "blue_move_down");
+		
+			// If player is touching the wall and the input direction is not changed stay still
+			if (_touchingWall && Math.Sign(_inputDir.Y) == Math.Sign(_lastInputDirY))
 			{
-				// check the proximity to the screen limits
-				if (Math.Abs(currentYPosition) > (Math.Abs(availableSpace) - _pixelBuffer) && StopOnWall)
+				Velocity = Vector2.Zero;
+				return;
+			}
+
+			// Unlock movement if input direction is changed
+			if (Math.Sign(_inputDir.Y) != Math.Sign(_lastInputDirY))
+			{
+				_touchingWall = false;
+			}
+
+			// Take in memory the last direction for next execution compare
+			_lastInputDirY = _inputDir.Y;
+
+			Velocity = _inputDir * Speed;
+
+			KinematicCollision2D collision = MoveAndCollide(Velocity * (float)delta);
+			if (collision != null)
+			{
+				// CollisionLayer 2 = Walls
+                if (collision.GetCollider() is PhysicsBody2D collidedBody && collidedBody.CollisionLayer == 2)
 				{
-					SHSound_CheckDirectionChange(currentYDirection);
+					AudioManager.Instance.PlaySound("PlayerHit");
+                    _touchingWall = true;
 
-					// if the user still wants to go in the direction of the screen limit, stop the paddle
-					if ((currentYDirection < 0 && currentYPosition < 0) || (currentYDirection > 0 && currentYPosition > 0))
-					{
-						currentVelocity.Y = 0;
-						SHSound_TryPlay(_SHSound_played);
-					}
-					else
-						currentVelocity.Y = currentYDirection * Speed;
-				}
-				else
-					currentVelocity.Y = currentYDirection * Speed;
-			}
-			else
-				currentVelocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-
-			Velocity = currentVelocity;
-			MoveAndSlide();
-
-			////////////// TOTALLY USELESS BUT WHY NOT /////////////////
-			if (Position.X != -565 && !_shootingStarsPlayed)
-			{
-				AudioManager.Instance.PlaySound("ShootingStars");
-				_shootingStarsPlayed = true;
-			}
-			////////////////////////////////////////////////////////////
-		}
-
-		private void SHSound_CheckDirectionChange(float currDir)
-		{
-			if (_SHSound_played)
-			{
-				// if the sound has been played check for input direction switched since the last play
-				if (_SHSound_lastDirection != currDir)
-				{
-					_SHSound_lastDirection = currDir;
-					_SHSound_played = false;
-				}
-			}
-		}
-
-		// The conditions to play the sound are:
-		// - the player hasn't yet hit the screen
-		// - the input direction is changed at least once since the last hit
-		private void SHSound_TryPlay(bool playable)
-		{
-			if (!playable)
-			{
-				AudioManager.Instance.PlaySound("PlayerHit");
-				_SHSound_played = true;
-			}
+                    // Take back the player to the correct position
+                    var remainder = collision.GetRemainder();
+                    Position -= remainder;
+                }
+            }
 		}
 	}
 }
